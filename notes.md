7565098bbf7242d3ccebc9d8408b2a11fbf30dfd
tonitdiep <adiep123@gmail.com>
2022-02-10 16:06:00 -0500

    updated notes, README, server.js

---
+++ notes.md
@@ -0,0 +1,6 @@
+#Personal Knowledge Learning Corner 
+
+## What is happening here? creating our server 
+Express.js res.send() function that sends HTTP response
+    https://www.geeksforgeeks.org/express-js-res-send-function/
+
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 
+app.length('/', (req, res) => {
+    res.send('Helloo World, from server.js')
+})
 app.listen(5000)
 
-//testing 
+/*
+What is happening here? creating our server 
+Express.js res.send() function that sends HTTP response
+    https://www.geeksforgeeks.org/express-js-res-send-function/
+
+*/
