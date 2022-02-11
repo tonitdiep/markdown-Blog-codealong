dda79f88e3ece5e96a5d19ff22fdc5fd9d717a78
tonitdiep <adiep123@gmail.com>
2022-02-10 20:12:58 -0500

    set up views folder with index.ejs

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+//view engine convert ejs code to html
+//ejs -here we will be writing our views using ejs
 app.get('/', (req, res) => {
     res.send('Helloo World, from server.js')
 })
